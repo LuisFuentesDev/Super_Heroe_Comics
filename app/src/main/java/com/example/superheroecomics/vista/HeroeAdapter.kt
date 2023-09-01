726f6861d234fbaed816865b7aff3ba5f51b7bc4
LuisFuentesDev <lsfuentes2@gmail.com>
2023-08-31 20:24:09 -0400

    Se crea el adapter y el layout del item

--- app/src/main/java/com/example/superheroecomics/vista/HeroeAdapter.kt
+++ app/src/main/java/com/example/superheroecomics/vista/HeroeAdapter.kt
@@ -1,4 +1,45 @@
 package com.example.superheroecomics.vista
 
-class HeroeAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.superheroecomics.data.local.HeroeEntity
+import com.example.superheroecomics.databinding.ItemHeroeBinding
+
+class HeroeAdapter : RecyclerView.Adapter<HeroeAdapter.ItemHeroeViewHolder>() {
+
+    lateinit var binding: ItemHeroeBinding
+    private val listItemHeroes = mutableListOf<HeroeEntity>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHeroeViewHolder {
+        binding = ItemHeroeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemHeroeViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return listItemHeroes.size
+    }
+
+    override fun onBindViewHolder(holder: ItemHeroeViewHolder, position: Int) {
+        val phone = listItemHeroes[position]
+        holder.bind(phone)
+    }
+
+    fun setData(phones: List<HeroeEntity>) {
+        this.listItemHeroes.clear()
+        this.listItemHeroes.addAll(phones)
+        notifyDataSetChanged()
+
+    }
+
+
+    class ItemHeroeViewHolder(val heroeVista: ItemHeroeBinding) :
+        RecyclerView.ViewHolder(heroeVista.root) {
+        fun bind(heroe: HeroeEntity) {
+            heroeVista.imageViewHeroe.load(heroe.imagenLink)
+            heroeVista.textViewName.text = heroe.nombre
+            heroeVista.textViewAO.text = heroe.Año_creacion.toString()
+        }
+    }
 }
